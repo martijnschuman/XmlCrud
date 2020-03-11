ef6fb0b45958ffae46a665ce7b18e3e138833d2d
Martijn Schuman <martijnschuman@hotmail.com>
2020-03-11 14:29:50 +0100

    Standaard bestanden toegevoegd

---
+++ App_Code/SongMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace XmlCrud.App_Code
+{
+    public class SongMethods
+    {
+        //Pagina 9
+        public DataSet GetAllSongs()
+        {
+            DataSet ds = new DataSet("playlist");
+
+            DataTable dtSongs = new DataTable("songs");
+
+            DataColumn dcId = new DataColumn("id");
+            DataColumn dcArtist = new DataColumn("artist");
+            DataColumn dcTitle = new DataColumn("title");
+
+            dtSongs.Columns.Add(dcId);
+            dtSongs.Columns.Add(dcArtist);
+            dtSongs.Columns.Add(dcTitle);
+            ds.Tables.Add(dtSongs);
+
+            ds.ReadXml(HttpContext.Current.Server.MapPath("~/App_Data/PlayList.xml"));
+
+            return ds;
+        }
+    }
+}

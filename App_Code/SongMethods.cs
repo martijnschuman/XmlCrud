d30f3fad8b29a4018dd9cf8531df9ab5febe14d7
BuildTools <unconfigured@null.spigotmc.org>
2020-03-11 19:30:07 +0100

    Playlist lezen, bekijken, nummer toevoegen

--- App_Code/SongMethods.cs
+++ App_Code/SongMethods.cs
@@ -8,25 +8,38 @@ namespace XmlCrud.App_Code
 {
     public class SongMethods
     {
-        //Pagina 9
-        public DataSet GetAllSongs()
-        {
-            DataSet ds = new DataSet("playlist");
+        DataSet ds = new DataSet("playlist");
 
+        public DataSet GetAllSongs(string file)
+        {
             DataTable dtSongs = new DataTable("songs");
 
             DataColumn dcId = new DataColumn("id");
             DataColumn dcArtist = new DataColumn("artist");
             DataColumn dcTitle = new DataColumn("title");
+            DataColumn dcFileLocation = new DataColumn("fileLocation");
 
             dtSongs.Columns.Add(dcId);
             dtSongs.Columns.Add(dcArtist);
             dtSongs.Columns.Add(dcTitle);
+            dtSongs.Columns.Add(dcFileLocation);
             ds.Tables.Add(dtSongs);
 
-            ds.ReadXml(HttpContext.Current.Server.MapPath("~/App_Data/PlayList.xml"));
+            ds.ReadXml(HttpContext.Current.Server.MapPath(file));
 
             return ds;
+        }
+
+        public DataRow GetEmptyDataRow()
+        {
+            DataRow dr = ds.Tables["songs"].NewRow();
+            return dr;
+        }
+
+        public void CreateSong(DataRow dataRow, string file)
+        {
+            ds.Tables["songs"].Rows.Add(dataRow);
+            ds.WriteXml(HttpContext.Current.Server.MapPath(file));
         }
     }
 }

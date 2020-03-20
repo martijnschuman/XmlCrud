b681d527ad29eeeee3b3a9e371292f15a28f2123
Martijn Schuman <martijnschuman@hotmail.com>
2020-03-20 02:51:20 +0100

    MusicPlayer bijgewerkt

--- Scripts/MusicPlayer.js
+++ Scripts/MusicPlayer.js
@@ -1,22 +1,54 @@
-﻿function ShowMusicPlayer(titleAtr, artistAtr, albumAtr, coverAtr, fileLocationAtr) {
+﻿/*Opent muziek speler*/
+function MusicPlayerOpen() {
+    document.getElementById("MusicPlayer").style.display = "block";
+    document.getElementById("MusicPlayerOpen").style.setProperty('display', 'none', 'important');
+}
+
+/*Sluit muziek speker*/
+function MusicPlayerClose() {
+    document.getElementById("MusicPlayer").style.display = "none";
+    document.getElementById("MusicPlayerOpen").style.setProperty('display', 'block', 'important');
+}
+
+/*Laat nummer informatie in de muziek speler zien*/
+function MusicPlayerInfo(titleAtr, artistAtr, albumAtr, coverAtr) {
 
     var title = document.getElementById("MusicPlayerTitle");
     var artist = document.getElementById("MusicPlayerArtist");
     var album = document.getElementById("MusicPlayerAlbum");
     var cover = document.getElementById("songCoverImage");
-    //var fileLocation = document.getElementById("MusicPlayerfileLocation");
-
     title.innerHTML = titleAtr;
     artist.innerHTML = artistAtr;
     album.innerHTML = albumAtr;
     cover.src = coverAtr;
-    //fileLocation.innerHTML = fileLocationAtr;
 }
 
 /*Volume*/
 function SetVolume(val) {
-    var player = document.getElementById('video');
-    console.log('Before: ' + player.volume);
+    var player = document.getElementById('audio');
     player.volume = val / 100;
-    console.log('After: ' + player.volume);
+}
+
+/*Veranderd play-pauze knop*/
+function MusicPlayerPlayPause() {
+    var play = document.getElementById("MusicPlayerPlay");
+
+    if (play.className == "fa fa-play PlayMusicButton") {
+        play.className = "fa fa-pause PlayMusicButton";
+        play.style.marginRight = "17px";
+    }
+    else if (play.className == "fa fa-pause PlayMusicButton") {
+        play.className = "fa fa-play PlayMusicButton";
+        play.style.marginRight = "20px";
+    }
+}
+
+/*Volgend nummer */
+function MusicPlayerPrevSong() {
+    console.log("Vorig nummer");
+}
+
+/*Vorig nummer */
+function MusicPlayerNextSong() {
+    console.log("Volgend nummer");
 }
